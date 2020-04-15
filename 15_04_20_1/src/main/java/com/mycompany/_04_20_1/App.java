7f4c3185f9d041fa8278ae8481d764e9d1e73846
mailnilaydas@gmail.com <mailnilaydas@gmail.com>
2020-04-15 11:10:44 +0530

    commit

---
+++ 15_04_20_1/src/main/java/com/mycompany/_04_20_1/App.java
@@ -0,0 +1,13 @@
+package com.mycompany._04_20_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ 15_04_20_1/src/main/java/com/mycompany/_04_20_1/CreateDemo.java
@@ -0,0 +1,73 @@
+package com.mycompany._04_20_1;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.mycompany._04_20_1.entity.Instructor;
+import com.mycompany._04_20_1.entity.InstructorDetail;
+
+public class CreateDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.buildSessionFactory();
+		
+		// create session
+		Session session = factory.openSession();
+		try {			
+			
+			// create the objects
+			/*
+			Instructor tempInstructor = 
+					new Instructor("Chad", "Darby", "darby@luv2code.com");
+			
+			InstructorDetail tempInstructorDetail =
+					new InstructorDetail(
+							"http://www.luv2code.com/youtube",
+							"Luv 2 code!!!");		
+			*/
+			
+			Instructor tempInstructor = 
+					new Instructor("Madhu", "Patel", "madhu@luv2code.com");
+			
+			InstructorDetail tempInstructorDetail =
+					new InstructorDetail(
+							"http://www.youtube.com",
+							"Guitar");		
+			
+			// associate the objects
+			tempInstructor.setInstructorDetail(tempInstructorDetail);
+			
+			// start a transaction
+			session.beginTransaction();
+			
+			// save the instructor
+			//
+			// Note: this will ALSO save the details object
+			// because of CascadeType.ALL
+			//
+			System.out.println("Saving instructor: " + tempInstructor);
+			session.save(tempInstructor);					
+			
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		}
+		finally {
+			factory.close();
+		}
+	}
+
+}
+
+
+
+
+
