67ac9fc2288f14f0a7387870dd254c5c8bf47994
nilay <nilay@nilay-HP-EliteBook-840-G2>
2020-04-21 17:45:27 +0530

    commit

--- 21_04_20_02/src/main/java/comm/example/rest/StudentRestController.java
+++ 21_04_20_02/src/main/java/comm/example/rest/StudentRestController.java
@@ -1,7 +1,6 @@
 package comm.example.rest;
 
 import java.util.ArrayList;
-import java.util.InputMismatchException;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -11,6 +10,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -53,6 +54,13 @@ public class StudentRestController {
 		
 		return theStudents.get(studentId);
 	
+	}
+	
+	@PostMapping("/students")
+	public Student createStudent(@RequestBody Student theStudent)
+	{
+		theStudents.add(theStudent);
+		return theStudent;
 	}
 	@ExceptionHandler
 		public ResponseEntity<StudentResponseEntity> handleException(StudentNotFoundException snfe)
