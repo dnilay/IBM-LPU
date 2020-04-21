75ec652811a4f35fb5d922ef8eca97347f9067d7
nilay <nilay@nilay-HP-EliteBook-840-G2>
2020-04-21 15:23:39 +0530

    cpmmit

--- 21_04_20_02/src/main/java/comm/example/config/DemoConfig.java
+++ 21_04_20_02/src/main/java/comm/example/config/DemoConfig.java
@@ -1,13 +1,31 @@
 package comm.example.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 @EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = "comm.example")
 public class DemoConfig implements WebMvcConfigurer{
+	
+	@Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/pages/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
 
 }
