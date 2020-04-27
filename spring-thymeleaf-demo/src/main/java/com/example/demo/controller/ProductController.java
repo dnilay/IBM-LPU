72d8247440f8d83c9220930ccece6e3a0102cce2
nilay <nilay@nilay-HP-EliteBook-840-G2>
2020-04-27 11:05:48 +0530

    commit

---
+++ spring-thymeleaf-demo/src/main/java/com/example/demo/controller/ProductController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.service.ProductService;
+
+@Controller
+public class ProductController {
+
+    private ProductService productService;
+
+    @Autowired
+    public void setProductService(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @RequestMapping("/products")
+    public String listProducts(Model model){
+
+        model.addAttribute("products", productService.listAllProducts());
+
+        return "products";
+    }
+}
---
+++ spring-thymeleaf-demo/src/main/java/com/example/demo/service/ProductService.java
@@ -0,0 +1,11 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.model.Product;
+
+
+public interface ProductService {
+
+    List<Product> listAllProducts();
+}
