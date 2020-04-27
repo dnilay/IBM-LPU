798683dd90bcbd610d2d6bc1e394a60881b4349b
nilay <nilay@nilay-HP-EliteBook-840-G2>
2020-04-27 12:08:09 +0530

    commit

--- spring-core-spring-mvc-create-product/src/main/java/guru/springframework/controllers/ProductController.java
+++ spring-core-spring-mvc-create-product/src/main/java/guru/springframework/controllers/ProductController.java
@@ -47,6 +47,6 @@ public class ProductController {
     @RequestMapping(value = "/product", method = RequestMethod.POST)
     public String saveOrUpdateProduct(Product product){
         Product savedProduct = productService.saveOrUpdateProduct(product);
-        return "redirect:/product/" + savedProduct.getId();
+        return "redirect:/products/";
     }
 }
