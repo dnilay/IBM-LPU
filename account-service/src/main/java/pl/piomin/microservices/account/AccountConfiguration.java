ec92634b34323443e0e2d8e5a4a05bb3d598f203
nilay <nilay@nilay-HP-EliteBook-840-G2>
2020-04-28 12:37:41 +0530

    commit

---
+++ account-service/src/main/java/pl/piomin/microservices/account/AccountConfiguration.java
@@ -0,0 +1,11 @@
+package pl.piomin.microservices.account;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("pl.piomin.microservices.account")
+public class AccountConfiguration {
+
+
+}
---
+++ gateway-service/src/main/java/pl/piomin/microservices/edge/Application.java
@@ -0,0 +1,18 @@
+package pl.piomin.microservices.edge;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableZuulProxy
+
+public class Application {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(Application.class).web(true).run(args);
+	}
+
+	
+}
