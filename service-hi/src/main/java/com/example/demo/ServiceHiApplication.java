660c9ce0b7c7a0b139914c825794e98e4b29a87c
nilay <nilay@nilay-HP-EliteBook-840-G2>
2020-04-28 10:22:11 +0530

    commit

---
+++ service-hi/src/main/java/com/example/demo/ServiceHiApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class ServiceHiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceHiApplication.class, args);
+	}
+	@Value("${server.port}")
+	String port;
+	@RequestMapping("/hi")
+	public String home(@RequestParam String name) {
+		return "hi "+name+",i am from port:" +port;
+	}
+}
