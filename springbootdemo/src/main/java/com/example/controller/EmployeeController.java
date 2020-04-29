d4a0acb25a85952f80bf62aeaa75cb689c967c63
nilay <nilay@nilay-HP-EliteBook-840-G2>
2020-04-29 13:04:51 +0530

    commit

---
+++ springbootdemo/src/main/java/com/example/controller/EmployeeController.java
@@ -0,0 +1,54 @@
+package com.example.controller;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.example.dao.EmployeeDAO;
+import com.example.model.Employee;
+import com.example.model.Employees;
+
+@RestController
+@RequestMapping(path = "/employees")
+public class EmployeeController 
+{
+    @Autowired
+    private EmployeeDAO employeeDao;
+    
+    @GetMapping(path="/", produces = "application/json")
+    public Employees getEmployees() 
+    {
+        return employeeDao.getAllEmployees();
+    }
+    
+    @PostMapping(path= "/", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<Object> addEmployee(
+     
+        @RequestBody Employee employee) 
+            throws Exception 
+    {       
+        //Generate resource id
+        Integer id = employeeDao.getAllEmployees().getEmployeeList().size() + 1;
+        employee.setId(id);
+        
+        //add resource
+        employeeDao.addEmployee(employee);
+        
+        //Create resource location
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                                    .path("/{id}")
+                                    .buildAndExpand(employee.getId())
+                                    .toUri();
+        
+        //Send location in response
+        return ResponseEntity.created(location).build();
+    }
+}
---
+++ springbootdemo/src/main/java/com/example/dao/EmployeeDAO.java
@@ -0,0 +1,28 @@
+package com.example.dao;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.model.Employee;
+import com.example.model.Employees;
+
+@Repository
+public class EmployeeDAO 
+{
+    private static Employees list = new Employees();
+    
+    static 
+    {
+        list.getEmployeeList().add(new Employee(1, "John", "Doe", "john@gmail.com"));
+        list.getEmployeeList().add(new Employee(2, "Alex", "Kolenchiskey", "abc@gmail.com"));
+        list.getEmployeeList().add(new Employee(3, "David", "Kameron", "titanic@gmail.com"));
+    }
+    
+    public Employees getAllEmployees() 
+    {
+        return list;
+    }
+    
+    public void addEmployee(Employee employee) {
+        list.getEmployeeList().add(employee);
+    }
+}
