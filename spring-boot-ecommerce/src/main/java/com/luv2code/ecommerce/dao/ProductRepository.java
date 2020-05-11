d7e6bd42d048422b74a5c71e38a81905e0b8d49d
nilay <mailnilaydas@gmail.com>
2020-05-11 12:56:49 +0530

    commit

--- spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
+++ spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -1,9 +1,16 @@
 package com.luv2code.ecommerce.dao;
 
-import com.luv2code.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.luv2code.ecommerce.entity.Product;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+	@Query
+	Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 }
